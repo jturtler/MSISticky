be33e5ab8f1afa1ee9d248df6b2064ac82e9a373
James Chang <jamesbchang@gmail.com>
2018-05-20 16:32:53 +0900

    Add 2 cron job files

---
+++ CronJobs/cron_MSI12_AggUpdate.js
@@ -0,0 +1,5 @@
+MAILTO=jamesbchang@gmail.com
+
+#Min	Hour	Date	Month	Day	User	Binary -options arguments > output direction
+
+30	04	*	*	*	root	/usr/bin/node /tmp/nodejs/MSI11_AggUpdate.js [logLevel]:1 [sourceType]:Cron
