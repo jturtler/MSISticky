d6f3ee69677f75e2298039b2404122c1bf8a92d4
Dave Palmer <david.palmer@mariestopes.org.uk>
2018-07-27 12:03:20 +0100

    refactor how we query events into a single sub query, so that we can union a list of events and order them together after

--- SqlViews/SqlView_MSI_11_Events.sql
+++ SqlViews/SqlView_MSI_11_Events.sql
@@ -1,47 +1,49 @@
 -- Part 1 : retrieve all events for org units the status program is assigned to
-select psi.uid
- , ou.uid
- , coalesce(to_char( psi.executiondate,'YYYYMM'),'201701')
- , coalesce(psi.executiondate,'2017-01-01 00:00:00.0')  -- eventdate
- , psi.programstageid
- , prevSts.value as "prevSts"
- , coalesce(newSts.value, '') as "newSts"
- , elapsDate.value as "elapsDate"
- , noteData.value as "noteData"
-
-from organisationunit ou
-
-  inner join program_organisationunits as prgorg
-    on prgorg.organisationunitid = ou.organisationunitid
-    and prgorg.programid = (select programid from program where uid = '${prgid}')
-
-  inner join program p 
-    on p.programid = prgorg.programid 
-
-  inner join programstage as ps
-    on p.programid = ps.programid
-  
-  inner join programstageinstance as psi
-    on psi.organisationunitid = ou.organisationunitid 
-      and psi.programstageid = ps.programstageid  
-
-  inner join trackedentitydatavalue as newSts
-    on psi.programstageinstanceid = newSts.programstageinstanceid
-      and newSts.dataelementid = (select dataelementid from dataelement where uid = 'XhFcLwoD1Dr' limit 1 ) -- 244950
-
-  left outer join trackedentitydatavalue as prevSts
-    on psi.programstageinstanceid = prevSts.programstageinstanceid
-      and prevSts.dataelementid = (select dataelementid from dataelement where uid = 'k95lcIlS4bv' limit 1 ) --786150
+SELECT * FROM (
+    SELECT
+       psi.uid AS programuid
+     , ou.uid AS ouuid
+     , coalesce(to_char( psi.executiondate,'YYYYMM'),'201701') AS period
+     , coalesce(psi.executiondate,'2017-01-01 00:00:00.0') AS eventdate
+     , psi.programstageid AS programstageid
+     , prevSts.value as "prevSts"
+     , coalesce(newSts.value, '') as "newSts"
+     , elapsDate.value as "elapsDate"
+     , noteData.value as "noteData"
+    
+    from organisationunit ou
+    
+      inner join program_organisationunits as prgorg
+        on prgorg.organisationunitid = ou.organisationunitid
+        and prgorg.programid = (select programid from program where uid = '${prgid}')
+    
+      inner join program p 
+        on p.programid = prgorg.programid 
+    
+      inner join programstage as ps
+        on p.programid = ps.programid
       
-  left outer join trackedentitydatavalue as elapsDate
-    on psi.programstageinstanceid = elapsDate.programstageinstanceid
-      and elapsDate.dataelementid = (select dataelementid from dataelement where uid = 'UrD7yr6JLEf' limit 1 ) --  786151
-
-  left outer join trackedentitydatavalue as noteData
-    on psi.programstageinstanceid = noteData.programstageinstanceid
-      and noteData.dataelementid = (select dataelementid from dataelement where uid = 'Yww3Z8MYo1e' limit 1 ) --  244949
-
-WHERE ( '${ouid}' = 'ALL' OR ou.path like '%/${ouid}%' )
-  AND ou.hierarchylevel = 6 
-
-order by ou.uid, psi.executiondate;
+      inner join programstageinstance as psi
+        on psi.organisationunitid = ou.organisationunitid 
+          and psi.programstageid = ps.programstageid  
+    
+      inner join trackedentitydatavalue as newSts
+        on psi.programstageinstanceid = newSts.programstageinstanceid
+          and newSts.dataelementid = (select dataelementid from dataelement where uid = 'XhFcLwoD1Dr' limit 1 ) -- 244950
+    
+      left outer join trackedentitydatavalue as prevSts
+        on psi.programstageinstanceid = prevSts.programstageinstanceid
+          and prevSts.dataelementid = (select dataelementid from dataelement where uid = 'k95lcIlS4bv' limit 1 ) --786150
+          
+      left outer join trackedentitydatavalue as elapsDate
+        on psi.programstageinstanceid = elapsDate.programstageinstanceid
+          and elapsDate.dataelementid = (select dataelementid from dataelement where uid = 'UrD7yr6JLEf' limit 1 ) --  786151
+    
+      left outer join trackedentitydatavalue as noteData
+        on psi.programstageinstanceid = noteData.programstageinstanceid
+          and noteData.dataelementid = (select dataelementid from dataelement where uid = 'Yww3Z8MYo1e' limit 1 ) --  244949
+    
+    WHERE ( '${ouid}' = 'ALL' OR ou.path like '%/${ouid}%' )
+      AND ou.hierarchylevel = 6 
+) all_events
+order by ouuid, eventdate;
--- SqlViews/SqlView_MSI_12_Events.sql
+++ SqlViews/SqlView_MSI_12_Events.sql
@@ -1,57 +1,59 @@
 -- Part 1 : retrieve all sub segmentation events for org units the seg program is assigned to
-select psi.uid
- , ou.uid
- , coalesce(to_char( psi.executiondate,'YYYYMM'),'201701')
- , coalesce(psi.executiondate,'2017-01-01 00:00:00.0')  -- eventdate
- , psi.programstageid
- , prevSts.value as "prevSts"
- , coalesce(newSts.value, 'UNK') as "newSts"
- , elapsDate.value as "elapsDate"
- , noteData.value as "noteData"
- , prevSubSts.value as "prevSubSts"
- , coalesce(newSubSts.value, 'UNK') as "newSubSts"
-
-from organisationunit ou
-
-  inner join program_organisationunits as prgorg
-    on prgorg.organisationunitid = ou.organisationunitid
-    and prgorg.programid = (select programid from program where uid = '${prgid}')
-
-  inner join program p 
-    on p.programid = prgorg.programid 
-
-  inner join programstage as ps
-    on p.programid = ps.programid
-  
-  inner join programstageinstance as psi
-    on psi.organisationunitid = ou.organisationunitid 
-      and psi.programstageid = ps.programstageid  
-
-  inner join trackedentitydatavalue as newSubSts
-    on psi.programstageinstanceid = newSubSts.programstageinstanceid
-      and newSubSts.dataelementid = (select dataelementid from dataelement where uid = 'DwXW311h36K' limit 1 ) --  2869119
-
-  left outer join trackedentitydatavalue as newSts
-    on psi.programstageinstanceid = newSts.programstageinstanceid
-      and newSts.dataelementid = (select dataelementid from dataelement where uid = 'wYoGZDnvu3n' limit 1 ) --  244951
-
-  left outer join trackedentitydatavalue as prevSts
-    on psi.programstageinstanceid = prevSts.programstageinstanceid
-      and prevSts.dataelementid = (select dataelementid from dataelement where uid = 'T2iHkAEidOd' limit 1 ) --  786154
-
-  left outer join trackedentitydatavalue as prevSubSts
-    on psi.programstageinstanceid = prevSubSts.programstageinstanceid
-      and prevSubSts.dataelementid = (select dataelementid from dataelement where uid = 'Lwugb7O0coU' limit 1 ) --  2869137
+SELECT * FROM (
+    SELECT 
+       psi.uid AS programuid
+     , ou.uid AS ouuid
+     , coalesce(to_char( psi.executiondate,'YYYYMM'),'201701') AS period
+     , coalesce(psi.executiondate,'2017-01-01 00:00:00.0') AS eventdate
+     , psi.programstageid AS programstageid
+     , prevSts.value as "prevSts"
+     , coalesce(newSts.value, 'UNK') as "newSts"
+     , elapsDate.value as "elapsDate"
+     , noteData.value as "noteData"
+     , prevSubSts.value as "prevSubSts"
+     , coalesce(newSubSts.value, 'UNK') as "newSubSts"
+    
+    from organisationunit ou
+    
+      inner join program_organisationunits as prgorg
+        on prgorg.organisationunitid = ou.organisationunitid
+        and prgorg.programid = (select programid from program where uid = '${prgid}')
+    
+      inner join program p 
+        on p.programid = prgorg.programid 
+    
+      inner join programstage as ps
+        on p.programid = ps.programid
       
-  left outer join trackedentitydatavalue as elapsDate
-    on psi.programstageinstanceid = elapsDate.programstageinstanceid
-      and elapsDate.dataelementid = (select dataelementid from dataelement where uid = 'UrD7yr6JLEf' limit 1 ) --  786151
-
-  left outer join trackedentitydatavalue as noteData
-    on psi.programstageinstanceid = noteData.programstageinstanceid
-      and noteData.dataelementid = (select dataelementid from dataelement where uid = 'Yww3Z8MYo1e' limit 1 ) --  244949
-
-WHERE ( '${ouid}' = 'ALL' OR ou.path like '%/${ouid}%' )
-  AND ou.hierarchylevel = 6 
-
-order by ou.uid, psi.executiondate;
+      inner join programstageinstance as psi
+        on psi.organisationunitid = ou.organisationunitid 
+          and psi.programstageid = ps.programstageid  
+    
+      inner join trackedentitydatavalue as newSubSts
+        on psi.programstageinstanceid = newSubSts.programstageinstanceid
+          and newSubSts.dataelementid = (select dataelementid from dataelement where uid = 'DwXW311h36K' limit 1 ) --  2869119
+    
+      left outer join trackedentitydatavalue as newSts
+        on psi.programstageinstanceid = newSts.programstageinstanceid
+          and newSts.dataelementid = (select dataelementid from dataelement where uid = 'wYoGZDnvu3n' limit 1 ) --  244951
+    
+      left outer join trackedentitydatavalue as prevSts
+        on psi.programstageinstanceid = prevSts.programstageinstanceid
+          and prevSts.dataelementid = (select dataelementid from dataelement where uid = 'T2iHkAEidOd' limit 1 ) --  786154
+    
+      left outer join trackedentitydatavalue as prevSubSts
+        on psi.programstageinstanceid = prevSubSts.programstageinstanceid
+          and prevSubSts.dataelementid = (select dataelementid from dataelement where uid = 'Lwugb7O0coU' limit 1 ) --  2869137
+          
+      left outer join trackedentitydatavalue as elapsDate
+        on psi.programstageinstanceid = elapsDate.programstageinstanceid
+          and elapsDate.dataelementid = (select dataelementid from dataelement where uid = 'UrD7yr6JLEf' limit 1 ) --  786151
+    
+      left outer join trackedentitydatavalue as noteData
+        on psi.programstageinstanceid = noteData.programstageinstanceid
+          and noteData.dataelementid = (select dataelementid from dataelement where uid = 'Yww3Z8MYo1e' limit 1 ) --  244949
+    
+    WHERE ( '${ouid}' = 'ALL' OR ou.path like '%/${ouid}%' )
+      AND ou.hierarchylevel = 6 
+) all_events 
+ORDER BY ouuid, eventdate;
